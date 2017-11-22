938d9c41ddc373406a67b9f58545f6453ad63b7d
Bayan Saidolda <33599063+Bayan98@users.noreply.github.com>
2017-11-22 22:29:40 +0900

    Delete kmp.cpp

--- kmp.cpp
+++
@@ -1,7 +0,0 @@
-inline void kmp(int *p, const string &s){
-    for(int i=1; i<s.size(); i++){
-        int j = p[i-1];
-        while(j && s[i] != s[j]) j = p[j-1];
-        p[i] = j + (s[i] == s[j]);
-    }
-}
